1901aeb0dff4716c619e7c3f228e1f37e0c860c5
Vijetha <vjyndrn2@illinois.edu>
2015-11-16 15:04:11 -0600

    Restructuring and adding PCA

--- classifier.py
+++ classifier.py
@@ -2,21 +2,27 @@ import csv
 import numpy as np
 import os
 
-from sklearn.svm import OneClassSVM
+from sklearn.svm import OneClassSVM as OCSVM
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 from scipy import stats
 
-class Classifier:
-    baseFeatureFolder = "features/"
-    outputFileName = "Output.csv"
+def writeCsv(clf, outputWriter):
+    for i in range(200):
+        outputWriter.writerow([clf.ids[i], clf.label[i]])
 
-    def __init__(self):
-        self.featuresHash = {}
-        self.outliers_fraction = 0.03
-        self.label = []
-        self.ids = []
+class Classifier(object):
+    baseFeatureFolder = "features/"
+
+    def __init__(self, clfName):
+        self.clfName = clfName
+        self.outputFileName = self.clfName+"_Output.csv"
 
     # Function to load the features from feature file
     def loadFeatures(self, _driverId):
+        self.featuresHash = {}
+        self.label = []
+        self.ids = []
         featureFileName = self.baseFeatureFolder + _driverId
         infile = open(featureFileName, 'r')
         infileReader = csv.reader(infile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
@@ -24,33 +30,71 @@ class Classifier:
         for line in infileReader:
             self.featuresHash[line[0]] = np.nan_to_num(np.array(map(float, line[1:])))
 
-    def runSVMClassifier(self):
+    def getPCA(self, X):
+        # normalize the features
+        X_std = StandardScaler().fit_transform(X)
+
+        # PCA
+        pca = PCA(n_components=7)
+        pca.fit(X_std)
+        return pca.transform(X_std)
+
+class OneClassSVM(Classifier):
+
+    def __init__(self, nu, gamma, runpca=False):
+        self.runpca = runpca
+        clfName = 'OneClassSVM'
+        if self.runpca:
+            clfName = clfName + "_PCA"
+        super(OneClassSVM, self).__init__(clfName)
+        self.outliers_fraction = 0.03
+        self.nu = nu
+        self.gamma = gamma
+
+    def runClassifier(self):
         X = self.featuresHash.values()
         self.ids = self.featuresHash.keys()
-        clf = OneClassSVM(nu=0.261, gamma=0.05)
+        if self.runpca:
+            X = self.getPCA(X)
+
+        clf = OCSVM(nu=self.nu, gamma=self.gamma)
         clf.fit(X)
         y_pred = clf.decision_function(X).ravel()
         threshold = stats.scoreatpercentile(y_pred, 100 * self.outliers_fraction)
         self.label = y_pred > threshold
         self.label = map(int, self.label)
 
-    def writeCsv(self, writer):
-        for i in range(200):
-            outputWriter.writerow([self.ids[i], self.label[i]])
-
-
 if __name__ == "__main__":
     driverData = os.listdir(Classifier.baseFeatureFolder)
-    outFile = open(Classifier.outputFileName, 'wb')
+
+    # Running One Class SVM without PCA
+    svmClf = OneClassSVM(0.261, 0.05)
+    outFile = open(svmClf.outputFileName, 'wb')
     outputWriter = csv.writer(outFile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
     outputWriter.writerow(['driver_trip', 'prob'])
+
     for driver in driverData:
         # sanity check to skip '.DS_Store' file in Mac
         if driver.startswith('.'):
             continue
         print "Processing driver ", driver
-        testClf = Classifier()
-        testClf.loadFeatures(driver)
-        testClf.runSVMClassifier()
-        testClf.writeCsv(outputWriter)
+        svmClf.loadFeatures(driver)
+        svmClf.runClassifier()
+        writeCsv(svmClf, outputWriter)
+    outFile.close()
+
+    # Running One Class SVM with PCA
+    svmClf = OneClassSVM(0.261, 0.05, True)
+    outFile = open(svmClf.outputFileName, 'wb')
+    outputWriter = csv.writer(outFile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    outputWriter.writerow(['driver_trip', 'prob'])
+
+    for driver in driverData:
+        # sanity check to skip '.DS_Store' file in Mac
+        if driver.startswith('.'):
+            continue
+        print "Processing driver ", driver
+        svmClf.loadFeatures(driver)
+        svmClf.runClassifier()
+        writeCsv(svmClf, outputWriter)
     outFile.close()
