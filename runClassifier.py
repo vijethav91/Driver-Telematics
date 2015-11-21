c9af2ca77a5ade35a4a1482de1f51d45f94fdc5d
moontails <dragontransformer@gmail.com>
2015-11-21 10:19:16 -0600

    Added factorymethod for classifier instantiation and support for command line args

--- runClassifier.py
+++ runClassifier.py
@@ -1,3 +1,4 @@
+import argparse
 import classifiers
 import csv
 import numpy as np
@@ -7,14 +8,39 @@ def writeCsv(clf, outputWriter):
     for i in range(200):
         outputWriter.writerow([clf.ids[i], clf.label[i]])
 
+def classifierFactory(name, runPCA, sampleType, numDrivers, numTrips):
+    if name == 'OneClassSVM': 
+        return classifiers.OneClassSVM(0.261, 0.05, runPCA)
+    if name == 'LogisticRegression': 
+        return classifiers.SimpleLogisticRegression(runPCA, sampleType, numDrivers, numTrips)
+
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--clf", help="specify the ML algorithm to be used", 
+                        choices=['OneClassSVM', 'LogisticRegression'])
+    parser.add_argument("-rp", "--runpca", help="choose to enable PCA ", action="store_true")
+    parser.add_argument("-s", "--sampleType", help="choose the sampling method to use", type=int, choices=[0, 1, 2])
+    parser.add_argument("-d", "--numDrivers", help="indicate the number of drivers", type=int, default=1)
+    parser.add_argument("-t", "--numTrips", help="indicate the number of trips", type=int, default=1)
+    parser.add_argument("-nc", "--numComponents", help="indicate the number of components for PCA", type=int, default=0)
+
+    return parser
+
 if __name__ == "__main__":
+    # Parse the command line args 
+    cmdParser = parseArgs()
+    args = cmdParser.parse_args()
+
     driverData = os.listdir(classifiers.Classifier.baseFeatureFolder)
 
+    # call the factory method to instantiate the appropriate classifier
+    clf = classifierFactory(args.clf, args.runpca, args.sampleType, args.numDrivers, args.numTrips)
+
     # Running Logistic Regression with PCA
-    slrClf = classifiers.SimpleLogisticRegression(runpca=True)
-    slrClf.loadAllFeatures(driverData)
+    if args.clf != 'OneClassSVM':
+        clf.loadAllFeatures(driverData)
 
-    outFile = open(slrClf.outputFileName, 'wb')
+    outFile = open(clf.outputFileName, 'wb')
     outputWriter = csv.writer(outFile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
     outputWriter.writerow(['driver_trip', 'prob'])
 
@@ -23,57 +49,9 @@ if __name__ == "__main__":
         if driver.startswith('.'):
             continue
         print "Processing driver ", driver
-        slrClf.runClassifier(driver, 1, 1, 1600, 50)
-        writeCsv(slrClf, outputWriter)
-    outFile.close()
-    exit()
 
-    # Running Logistic Regression without PCA
-    slrClf = classifiers.SimpleLogisticRegression()
-    slrClf.loadAllFeatures(driverData)
-    
-    outFile = open(slrClf.outputFileName, 'wb')
-    outputWriter = csv.writer(outFile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    outputWriter.writerow(['driver_trip', 'prob'])
-
-    for driver in driverData:
-        # sanity check to skip '.DS_Store' file in Mac
-        if driver.startswith('.'):
-            continue
-        print "Processing driver ", driver
-        slrClf.runClassifier(driver, 1, 1, 1600)
-        writeCsv(slrClf, outputWriter)
-    outFile.close()
-    exit()
-
-    # Running One Class SVM without PCA
-    svmClf = classifiers.OneClassSVM(0.261, 0.05)
-    outFile = open(svmClf.outputFileName, 'wb')
-    outputWriter = csv.writer(outFile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    outputWriter.writerow(['driver_trip', 'prob'])
-
-    for driver in driverData:
-        # sanity check to skip '.DS_Store' file in Mac
-        if driver.startswith('.'):
-            continue
-        print "Processing driver ", driver
-        svmClf.loadFeatures(driver)
-        svmClf.runClassifier()
-        writeCsv(svmClf, outputWriter)
-    outFile.close()
-
-    # Running One Class SVM with PCA
-    svmClf = classifiers.OneClassSVM(0.261, 0.05, True)
-    outFile = open(svmClf.outputFileName, 'wb')
-    outputWriter = csv.writer(outFile, delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    outputWriter.writerow(['driver_trip', 'prob'])
-
-    for driver in driverData:
-        # sanity check to skip '.DS_Store' file in Mac
-        if driver.startswith('.'):
-            continue
-        print "Processing driver ", driver
-        svmClf.loadFeatures(driver)
-        svmClf.runClassifier(7)
-        writeCsv(svmClf, outputWriter)
+        if args.clf == 'OneClassSVM':
+            clf.loadFeatures(driver)
+        clf.runClassifier(driver, args.numComponents)
+        writeCsv(clf, outputWriter)
     outFile.close()
